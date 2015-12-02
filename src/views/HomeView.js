0927ca0b201315f5a35307c475655404e1b8fa7b
queryxl <xubuild@gmail.com>
2015-12-02 10:42:25 +0100

    remove bootstrap class

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -24,11 +24,10 @@ export class HomeView extends React.Component {
 
   render() {
     return (
-      <div className='container text-center'>
+      <div>
         <h1>Welcome to the React Redux Starter Kit</h1>
         <h2>Sample Counter: {this.props.counter}</h2>
-        <button className='btn btn-default'
-                onClick={this.props.actions.increment}>
+        <button onClick={this.props.actions.increment}>
           Increment
         </button>
         <hr />
